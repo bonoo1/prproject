87d69ba79b2e47fb23f4a3f99911275c6e3088b7
bonoo74 <bonoo74@gmail.com>
2022-03-08 10:55:51 +0900

    순서 정리 및 글 작성창, 회원 가입창 route 생성

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 SECRET_KEY = 'SPARTA'
 
 
+##  HTML ##
+
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
@@ -40,9 +42,34 @@ def login():
     return render_template('login.html', msg=msg)
 
 
+@app.route('/signup')
+def sighup():
+    return render_template('signup.html',)
+
+
+@app.route('/write')
+def write():
+    return render_template('upload.html')
+
+    #회원가입##
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+
+        username_receive = request.form['username_give']
+        password_receive = request.form['password_give']
+        password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+        doc = {
+            "username": username_receive,  # 아이디
+            "password": password_hash,  # 비밀번호
+            "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        }
+        db.users.insert_one(doc)
+        return render_template('login.html')
+
+        # 로그인 ##
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-        # 로그인
+
         username_receive = request.form['username_give']
         password_receive = request.form['password_give']
 
@@ -62,21 +89,6 @@ def sign_in():
             return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-@app.route('/sign_up/save', methods=['POST'])
-def sign_up():
-    #회원가입
-        username_receive = request.form['username_give']
-        password_receive = request.form['password_give']
-        password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-        doc = {
-            "username": username_receive,  # 아이디
-            "password": password_hash,  # 비밀번호
-            "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
-        }
-        db.users.insert_one(doc)
-        return jsonify({'result': 'success'})
-
-
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():#ID 중복체크
     username_receive = request.form['username_give']
@@ -90,7 +102,7 @@ def posting():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.users.find_one({"username": payload["id"]})
-        song_receive = request.form["song_give"]
+        ytburl_receive = request.form["ytburl_give"]
         date_receive = request.form["date_give"]
 
         file = request.files["file_give"]
@@ -107,7 +119,7 @@ def posting():
         doc = {
             "username": user_info["username"],
             "profile_name": user_info["profile_name"],
-            "song": song_receive,    #노래url
+            "ytburl": ytburl_receive,    #노래url
             'file':f'{filename}.{extension}',
             "date": date_receive
         }
